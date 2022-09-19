7db40e3c03d90e2826d43cef92054324dd26fdc6
Benxima2 <nsimaessien63@yahoo.com>
2022-09-19 13:50:15 -0700

    go

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,11 @@
-#include <main.h>
+#include <unistd.h>
 
 /**
  * _putchar - write the character c to stdout
  * @c: the character to print
  *
  * Return on success 1.
- * On error, -1 is returned, and errn o is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
