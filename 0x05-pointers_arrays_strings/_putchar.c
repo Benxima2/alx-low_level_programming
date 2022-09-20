e91f4c4d23f31636fd66721834b63982b8ebed59
Benxima2 <nsimaessien63@yahoo.com>
2022-09-20 09:49:37 -0700

    putchar

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -4,7 +4,7 @@
  * _putchar - write the character c to stdout
  * @c: the character to print
  *
- * Return on success 1.
+ * Return: on success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
