effeaf7e14063e6c109f79cf471c56342fa3c5cd
Benxima2 <nsimaessien63@yahoo.com>
2022-10-04 07:11:02 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,7 +27,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int dest_len, a;
 
-	dest_len = _str_len(dest);
+	dest_len = _strlen(dest);
 	for (a = 0; a < n && src[a] != '\0'; a++)
 		dest[dest_len + a] = src[a];
 	return (dest);
