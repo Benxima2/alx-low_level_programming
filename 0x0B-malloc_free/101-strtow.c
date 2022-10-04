889c1cb3f7644a5c6b09f600c1c5796a72b2da2c
Benxima2 <nsimaessien63@yahoo.com>
2022-10-04 14:23:17 -0700

    go

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -36,7 +36,7 @@ void mallocmem(char **newstr, char *str, int str_len)
 				1++, word_len++;
 			newstr[j] = malloc(sizeof(char) * word_len);
 			newstr[j][word_len] = '\0';
-			j++, word_le = 1;
+			j++, word_len = 1;
 		}
 		i++;
 	}
